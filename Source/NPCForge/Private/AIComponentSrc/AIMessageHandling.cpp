663c12fc3f23630e0ddfaad48efe69bf1ebd66a2
Tom Daniel <tagemania@gmail.com>
2025-02-14 03:00:55 +0900

    [Feat]: small talk to

--- Source/NPCForge/Private/AIComponentSrc/AIMessageHandling.cpp
+++ Source/NPCForge/Private/AIComponentSrc/AIMessageHandling.cpp
@@ -22,6 +22,11 @@ void UAIComponent::HandleMessage(FMessage Message)
 	if (Message.ReceiverChecksum == EntityChecksum)
 	{
 		UE_LOG(LogTemp, Display, TEXT("[UAIComponent::HandleMessage]: %s received from %s : %s"), *EntityChecksum, *Message.SenderChecksum, *Message.Content);
-		// WebSocketHandler->SendMessage("TakeDecision", *Message.Content);
+		TSharedPtr<FJsonObject> JsonBody = MakeShareable(new FJsonObject());
+		JsonBody->SetStringField("message", Message.Content);
+		WebSocketHandler->SendMessage("TalkTo", JsonBody);
+
+		// its not just about that, we must also send the checksum of the sender and the receiver so the AI says who it wanna talk to.
+		// We must add in the system prompt (currently UE AI prompt) the fact that the AI should says who to talk to.
 	}
 }
